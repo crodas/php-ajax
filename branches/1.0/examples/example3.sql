90ce3664fb5f33692b718e7ff1d5554ea5c19274
saddor <saddor@d366d153-0b40-0410-9bbb-0d3318b94265>
2007-12-29 03:00:14 +0000

    + Fixing bug in PHP4 with json

--- branches/1.0/examples/example3.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `chat` (
-  `postid` int(11) NOT NULL auto_increment,
-  `userid` varchar(32) NOT NULL,
-  `postip` varchar(20)  NOT NULL,
-  `postcontent` varchar(250)  NOT NULL,
-  PRIMARY KEY  (`postid`)
-) ENGINE=MEMORY ;
